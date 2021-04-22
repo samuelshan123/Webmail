9dafa85f0ec2b6a06a8cec52df08c9ee246026ba
samuel  shan <samuelshan123@gmail.com>
2021-04-22 11:36:15 +0530

    Initial commit

---
+++ app/src/main/java/in/samuel/webmail/MainActivity.java
@@ -0,0 +1,57 @@
+package in.samuel.webmail;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebStorage;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Please Wait...");
+        progressDialog.setCancelable(false);
+        WebView web_view = findViewById(R.id.web_view);
+
+        web_view.requestFocus();
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.getSettings().getSavePassword();
+        web_view.getSettings().setSaveFormData(true);
+        web_view.getSettings().setSavePassword(true);
+        web_view.loadUrl("http://stellamaryscoe.edu.in:2095/");
+
+        web_view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        web_view.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+    }
+    
+}
